f0ddd9ff5876574eab932a21585a55614338b049
macsen110 <839945193@qq.com>
2018-07-29 22:08:32 +0800

    test auto update

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
 const electron = require('electron');
 const { app, BrowserWindow } = electron;
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
 const events = require('./events/index.js');
 const path = require('path');
 
 const {registEvents} = require('./tunnel/index.js');
 
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+
+
 app.on('ready', () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, './index.js'),
-      plugins: true
+      plugins: true,
+      nodeIntegration: false,
     }
   });
 
